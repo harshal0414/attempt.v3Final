144ca543b2448e55747b13d443572aeda378e844
Harshal Bhimartwar <92713292+harshal0414@users.noreply.github.com>
2024-05-02 00:43:03 +0530

    Update home_page.dart

--- lib/features/usr_auth/presentation/pages/home_page.dart
+++ lib/features/usr_auth/presentation/pages/home_page.dart
@@ -1,54 +1,25 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            SizedBox(height: 20,),
-            Container(
-              height: 130,
-              width: 130,
-              child: Image(image: NetworkImage('https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='),
-                loadingBuilder: (context, child, loadingProgress){
-                  return Center(child: CircularProgressIndicator()) ;
-                },
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/profile.png'), // Provide the path to your PNG image
+                fit: BoxFit.cover,
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+
+        ],
       ),
     );
   }
-
-
-
 }
-
-
-// import 'package:flutter/material.dart';
-//
-// class ProfileScreen extends StatefulWidget {
-//   const ProfileScreen({super.key});
-//
-//   @override
-//   State<ProfileScreen> createState() => _ProfileScreenState();
-// }
-//
-// class _ProfileScreenState extends State<ProfileScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//
-//     );
-//   }
-// }
